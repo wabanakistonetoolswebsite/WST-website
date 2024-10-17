e96e89695963268c1dd0ce0dee1556105974424b
SlugboieExtra <98238319+SlugboieExtra@users.noreply.github.com>
2024-10-17 20:04:26 -0300

    Update index.tsx

--- src/components/sections/GenericSection/index.tsx
+++ src/components/sections/GenericSection/index.tsx
@@ -14,7 +14,7 @@ export default function GenericSection(props) {
     const flexDirection = styles?.self?.flexDirection ?? 'row';
     const alignItems = styles?.self?.alignItems ?? 'flex-start';
     const hasTextContent = !!(badge?.url || title?.text || subtitle || text || actions.length > 0);
-    const hasMedia = !!(media && (media?.url || (media?.fields ?? []).length > 0) || (media?.__metadata?.modelname === "CardBlock"));
+    const hasMedia = !!(media && (media?.url || (media?.fields ?? []).length > 0) || (media?.title));
     const hasXDirection = flexDirection === 'row' || flexDirection === 'row-reverse';
 
     return (
